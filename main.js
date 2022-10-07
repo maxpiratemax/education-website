7573727d8f5a26a4462e2183d8652f81444b94da
Max <max_pirate@mail.ru>
2022-10-07 16:17:02 +0300

    added sections courses, faq, testimonials, footer

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 /* Change navbar color */
 const navBar = document.querySelector('nav')
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
     navBar.classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+/* Show/hide FAQ answer */
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'fa-solid fa-plus') {
+            icon.className = 'fa-solid fa-minus'
+        } else {
+            icon.className = 'fa-solid fa-plus'
+        }
+    })
+})
+
+/* Swiper slider */
+const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 })
