35c8c842666440c19907b7dd3c9af862ba6c8ec2
Max <max_pirate@mail.ru>
2022-10-11 00:25:10 +0300

    Added adaptivity, page about

--- main.js
+++ main.js
@@ -41,3 +41,20 @@ const swiper = new Swiper('.mySwiper', {
         prevEl: ".swiper-button-prev",
     },
 })
+
+/* Show/Hide nav menu */
+const menu = document.querySelector('.nav__menu')
+const menuOpenBtn = document.querySelector('#open-menu-btn')
+const menuCloseBtn = document.querySelector('#close-menu-btn')
+
+menuOpenBtn.addEventListener('click', () => {
+    menu.style.display = 'flex'
+    menuCloseBtn.style.display = 'inline-block'
+    menuOpenBtn.style.display = 'none'
+})
+
+menuCloseBtn.addEventListener('click', () => {
+    menu.style.display = 'none'
+    menuCloseBtn.style.display = 'none'
+    menuOpenBtn.style.display = 'inline-block'
+})
